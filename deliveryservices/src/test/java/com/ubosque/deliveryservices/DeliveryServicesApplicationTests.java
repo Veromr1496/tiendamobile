30d3b33c32b72fa50737c1eddcfdf7d1ced84fec
Verónica Merchán Rodríguez <veromr1496@gmail.com>
2021-11-02 22:29:42 -0500

    Base para proyecto de mobile

---
+++ deliveryservices/src/test/java/com/ubosque/deliveryservices/DeliveryServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubosque.deliveryservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
