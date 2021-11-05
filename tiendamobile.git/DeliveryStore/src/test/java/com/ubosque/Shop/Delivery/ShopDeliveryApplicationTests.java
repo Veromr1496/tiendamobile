b00e3438fedb2c1ab047f556c8e2dfd88dd851ef
Verónica Merchán Rodríguez <veromr1496@gmail.com>
2021-11-04 20:24:38 -0500

    Nueva del proyecto de mobile con backend

---
+++ tiendamobile.git/DeliveryStore/src/test/java/com/ubosque/Shop/Delivery/ShopDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubosque.Shop.Delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
